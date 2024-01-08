05836ba307964d120c21d171a26fbefc148dbbd6
berluk12@gmail.com <berluk12@gmail.com>
2024-01-08 15:01:31 +0100

    migrating description to lob

--- src/main/resources/db/prod/V4__migrating_description_to_description_html.sql
+++ src/main/resources/db/prod/V4__migrating_description_to_description_html.sql
@@ -1,7 +1,7 @@
-UPDATE PROJECT p SET p.desciprion_html = p.description;
-UPDATE TASK t SET t.desciprion_html = t.description;
-UPDATE PROJECT_HISTORY ph SET ph.desciprion_html = ph.description;
-UPDATE TASK_HISTORY th SET th.desciprion_html = th.description;
+UPDATE PROJECT p SET p.desciprion_html = CAST(p.description AS CLOB);
+UPDATE TASK t SET t.desciprion_html = CAST(t.description AS CLOB);
+UPDATE PROJECT_HISTORY ph SET ph.desciprion_html = CAST(ph.description AS CLOB);
+UPDATE TASK_HISTORY th SET th.desciprion_html = CAST(th.description AS CLOB);
 
 UPDATE PROJECT p SET p.description = null;
 UPDATE TASK t SET t.description = null;
